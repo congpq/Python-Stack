b4e50d80ef2d9c9e988bc4e64b6253078e7a7955
congpq <congpq@yeah.net>
2017-05-19 00:40:17 +0800

    更新文件
    有新增的学习内容 2017.05.19

--- 集合.py
+++ 集合.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+#学习方法集  builtins.py
+
 list_1 = [1,4,5,7,3,6,7,9]
 #set是强制修改为集合的类型    int数据整型
 list_1 = set(list_1)
@@ -29,4 +31,19 @@ print( list_1 ^ list_2 )
 print('+++++++++++++++++++++++++++')
 #两个集合有交集就返回False，无交集返回True
 print( list_1.isdisjoint(list_2) )
-
+print('----------------------')
+list_1.add(999)
+list_1.update([888,555,777])
+print(list_1)
+#remove如果值不存在set中将抛出异常
+list_1.remove(555)
+print(list_1)
+#显示集合的长度
+print( len(list_1) )
+#888是否在集合list_1中  #列表、集合、字符串判断是否存在都是这么写
+print( 888 in list_1)
+print( 888 not in list_1)
+#任意删除list_1中的某个元素并返回删除掉的这个元素
+print( list_1.pop() )
+#如果删除的元素不在集合中将不抛出异常
+print( list_1.discard(0000))
