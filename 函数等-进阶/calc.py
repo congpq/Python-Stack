5b13f505782b3641ac0ae2687f8071f8e364cdf9
congpq <congpq@yeah.net>
2017-05-24 23:42:48 +0800

    2017-05-24
    NEW 2017-05-24 second!

--- 函数等-进阶/calc.py
+++ 函数等-进阶/calc.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+#递归
 def calc(n):
     print(n)
     if int(n/2) > 0:
-        return calc( int(n/2))
+        return calc(int(n/2))
     print("-->", n)
 
 calc(10)
+
+#高阶函数
+def add(a, b, f):
+    return f(a) + f(b)
+
+res = add(3, -6, abs)
+print(res)
